612ba329702c144144dbcafecbbb557f91239732
not-thing <dade.dd@quarkonix.com>
2023-10-06 16:54:24 +0900

    update contract interface and deployments Fixes #17

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
-import { chromaticLpABI, chromaticLpRegistryABI, ierc20MetadataABI } from "./gen";
+import { chromaticLpRegistryABI, iChromaticLpABI, ierc20MetadataABI } from "./gen";
 import { Contract } from "./utils/helpers";
 
 /** @ignore */
-export interface ContractChromaticLP extends Contract<typeof chromaticLpABI> {}
+export interface ContractChromaticLP extends Contract<typeof iChromaticLpABI> {}
 
 /** @ignore */
 export interface ContractChromaticLPRegistry extends Contract<typeof chromaticLpRegistryABI> {}
