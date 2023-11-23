d7bf1a74939753223a9e3ec5461844b1ba18f1b7
daniel-fi <daniel-fi@users.noreply.github.com>
2023-11-23 09:22:59 +0000

    feat: add contract for boosting LP

--- src/gen/index.ts
+++ src/gen/index.ts
@@ -1,4 +1,114 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ChromaticBPFactory
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * [__View Contract on Arbitrum Goerli Arbiscan__](https://goerli.arbiscan.io//address/0x47Ff886d8406B1b4F3371F5aa3b64A72954B9d94)
+ */
+export const chromaticBpFactoryABI = [
+  { stateMutability: 'nonpayable', type: 'constructor', inputs: [] },
+  { type: 'error', inputs: [], name: 'OnlyAccessableByOwner' },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'lp', internalType: 'address', type: 'address', indexed: true },
+      { name: 'bp', internalType: 'address', type: 'address', indexed: false }
+    ],
+    name: 'ChromaticBPCreated'
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'previousOwner', internalType: 'address', type: 'address', indexed: true },
+      { name: 'newOwner', internalType: 'address', type: 'address', indexed: true }
+    ],
+    name: 'OwnershipTransferred'
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'bpList',
+    outputs: [{ name: 'bpAddresses', internalType: 'address[]', type: 'address[]' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [{ name: 'lp', internalType: 'address', type: 'address' }],
+    name: 'bpListByLP',
+    outputs: [{ name: 'bpAddresses', internalType: 'address[]', type: 'address[]' }]
+  },
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [
+      {
+        name: 'config',
+        internalType: 'struct BPConfig',
+        type: 'tuple',
+        components: [
+          { name: 'lp', internalType: 'contract IChromaticLP', type: 'address' },
+          { name: 'minRaisingTarget', internalType: 'uint256', type: 'uint256' },
+          { name: 'maxRaisingTarget', internalType: 'uint256', type: 'uint256' },
+          { name: 'startTimeOfWarmup', internalType: 'uint256', type: 'uint256' },
+          { name: 'durationOfWarmup', internalType: 'uint256', type: 'uint256' },
+          { name: 'durationOfLockup', internalType: 'uint256', type: 'uint256' }
+        ]
+      },
+      {
+        name: 'automateParam',
+        internalType: 'struct AutomateParam',
+        type: 'tuple',
+        components: [
+          { name: 'automate', internalType: 'address', type: 'address' },
+          { name: 'opsProxyFactory', internalType: 'address', type: 'address' }
+        ]
+      }
+    ],
+    name: 'createBP',
+    outputs: []
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'owner',
+    outputs: [{ name: '', internalType: 'address', type: 'address' }]
+  },
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: []
+  },
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [{ name: 'newOwner', internalType: 'address', type: 'address' }],
+    name: 'transferOwnership',
+    outputs: []
+  }
+] as const
+
+/**
+ * [__View Contract on Arbitrum Goerli Arbiscan__](https://goerli.arbiscan.io//address/0x47Ff886d8406B1b4F3371F5aa3b64A72954B9d94)
+ */
+export const chromaticBpFactoryAddress = {
+  421613: '0x47Ff886d8406B1b4F3371F5aa3b64A72954B9d94'
+} as const
+
+/**
+ * [__View Contract on Arbitrum Goerli Arbiscan__](https://goerli.arbiscan.io//address/0x47Ff886d8406B1b4F3371F5aa3b64A72954B9d94)
+ */
+export const chromaticBpFactoryConfig = {
+  address: chromaticBpFactoryAddress,
+  abi: chromaticBpFactoryABI
+} as const
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ChromaticLPRegistry
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -122,6 +232,271 @@ export const chromaticLpRegistryConfig = {
   address: chromaticLpRegistryAddress,
   abi: chromaticLpRegistryABI
 } as const
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// IChromaticBP
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const iChromaticBpABI = [
+  { type: 'error', inputs: [], name: 'BoostingNotExecuted' },
+  { type: 'error', inputs: [], name: 'BoostingNotSettled' },
+  { type: 'error', inputs: [], name: 'ClaimBalanceZeroError' },
+  { type: 'error', inputs: [], name: 'ClaimTimeError' },
+  { type: 'error', inputs: [], name: 'FullyRaised' },
+  { type: 'error', inputs: [], name: 'InvalidLockup' },
+  { type: 'error', inputs: [], name: 'InvalidRaisingTarget' },
+  { type: 'error', inputs: [], name: 'InvalidWarmup' },
+  { type: 'error', inputs: [], name: 'NotAutomationCalled' },
+  { type: 'error', inputs: [], name: 'NotRefundablePeriod' },
+  { type: 'error', inputs: [], name: 'NotWarmupPeriod' },
+  { type: 'error', inputs: [], name: 'RefundError' },
+  { type: 'error', inputs: [], name: 'RefundZeroAmountError' },
+  { type: 'error', inputs: [], name: 'StartTimeError' },
+  { type: 'error', inputs: [], name: 'TooSmallMinRaisingTarget' },
+  { type: 'error', inputs: [], name: 'ZeroDepositError' },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'owner', internalType: 'address', type: 'address', indexed: true },
+      { name: 'spender', internalType: 'address', type: 'address', indexed: true },
+      { name: 'value', internalType: 'uint256', type: 'uint256', indexed: false }
+    ],
+    name: 'Approval'
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'provider', internalType: 'address', type: 'address', indexed: true },
+      { name: 'bpTokenAmount', internalType: 'uint256', type: 'uint256', indexed: false },
+      { name: 'lpTokenAmount', internalType: 'uint256', type: 'uint256', indexed: false }
+    ],
+    name: 'BPClaimed'
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'provider', internalType: 'address', type: 'address', indexed: true },
+      { name: 'amount', internalType: 'uint256', type: 'uint256', indexed: false }
+    ],
+    name: 'BPDeposited'
+  },
+  { type: 'event', anonymous: false, inputs: [], name: 'BPExecuted' },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'provider', internalType: 'address', type: 'address', indexed: true },
+      { name: 'amount', internalType: 'uint256', type: 'uint256', indexed: false }
+    ],
+    name: 'BPRefunded'
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [{ name: 'totalLPToken', internalType: 'uint256', type: 'uint256', indexed: false }],
+    name: 'BPSettleUpdated'
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'from', internalType: 'address', type: 'address', indexed: true },
+      { name: 'to', internalType: 'address', type: 'address', indexed: true },
+      { name: 'value', internalType: 'uint256', type: 'uint256', indexed: false }
+    ],
+    name: 'Transfer'
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [
+      { name: 'owner', internalType: 'address', type: 'address' },
+      { name: 'spender', internalType: 'address', type: 'address' }
+    ],
+    name: 'allowance',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }]
+  },
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [
+      { name: 'spender', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' }
+    ],
+    name: 'approve',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [{ name: 'account', internalType: 'address', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }]
+  },
+  { stateMutability: 'nonpayable', type: 'function', inputs: [], name: 'boostLP', outputs: [] },
+  { stateMutability: 'nonpayable', type: 'function', inputs: [], name: 'boostLPTask', outputs: [] },
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [],
+    name: 'claimLiquidity',
+    outputs: []
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'currentPeriod',
+    outputs: [{ name: 'period', internalType: 'enum BPPeriod', type: 'uint8' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'decimals',
+    outputs: [{ name: '', internalType: 'uint8', type: 'uint8' }]
+  },
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [{ name: 'amount', internalType: 'uint256', type: 'uint256' }],
+    name: 'deposit',
+    outputs: []
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'endTimeOfLockup',
+    outputs: [{ name: 'timestamp', internalType: 'uint256', type: 'uint256' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'endTimeOfWarmup',
+    outputs: [{ name: 'timestamp', internalType: 'uint256', type: 'uint256' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'isClaimable',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'isDepositable',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'isRefundable',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'maxRaisingTarget',
+    outputs: [{ name: 'amount', internalType: 'uint256', type: 'uint256' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'minRaisingTarget',
+    outputs: [{ name: 'amount', internalType: 'uint256', type: 'uint256' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'name',
+    outputs: [{ name: '', internalType: 'string', type: 'string' }]
+  },
+  { stateMutability: 'nonpayable', type: 'function', inputs: [], name: 'refund', outputs: [] },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'resolveBoostLPTask',
+    outputs: [
+      { name: 'upkeepNeeded', internalType: 'bool', type: 'bool' },
+      { name: 'performData', internalType: 'bytes', type: 'bytes' }
+    ]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'settlementToken',
+    outputs: [{ name: 'token', internalType: 'contract IERC20', type: 'address' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'startTimeOfWarmup',
+    outputs: [{ name: 'timestamp', internalType: 'uint256', type: 'uint256' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'symbol',
+    outputs: [{ name: '', internalType: 'string', type: 'string' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'targetLP',
+    outputs: [{ name: 'lpAddress', internalType: 'contract IChromaticLP', type: 'address' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'totalRaised',
+    outputs: [{ name: 'amount', internalType: 'uint256', type: 'uint256' }]
+  },
+  {
+    stateMutability: 'view',
+    type: 'function',
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }]
+  },
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [
+      { name: 'to', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' }
+    ],
+    name: 'transfer',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }]
+  },
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [
+      { name: 'from', internalType: 'address', type: 'address' },
+      { name: 'to', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' }
+    ],
+    name: 'transferFrom',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }]
+  }
+] as const
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // IChromaticLP
