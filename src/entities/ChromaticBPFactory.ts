7f3f9d430533ee872b9719dc87915ed989a0122f
not-thing <dade.dd@quarkonix.com>
2023-11-28 12:04:13 +0900

    chore: fix typo

--- src/entities/ChromaticBPFactory.ts
+++ src/entities/ChromaticBPFactory.ts
@@ -7,10 +7,10 @@ import { handleBytesError } from "../utils/helpers";
 export class ChromaticBPFactory {
   constructor(private readonly _client: Client) {}
 
-  private getContract(registryAddress?: Address): ContractChromaticBPFactory {
+  private getContract(factoryAddress?: Address): ContractChromaticBPFactory {
     return getContract({
       address:
-        registryAddress ||
+        factoryAddress ||
         (chromaticBpFactoryAddress as Record<number, Address>)[
           this._client.publicClient?.chain?.id || 0
         ],
@@ -22,20 +22,20 @@ export class ChromaticBPFactory {
 
   contracts() {
     return {
-      registry: this.getContract(),
+      factory: this.getContract(),
     };
   }
 
   async bpList(): Promise<readonly Address[]> {
     return await handleBytesError(async () => {
-      return await this.contracts().registry.read.bpList({
+      return await this.contracts().factory.read.bpList({
         account: this._client.publicClient?.account,
       });
     });
   }
   async bpListByLP(marketAddress: Address): Promise<readonly Address[]> {
     return await handleBytesError(async () => {
-      return await this.contracts().registry.read.bpListByLP([marketAddress], {
+      return await this.contracts().factory.read.bpListByLP([marketAddress], {
         account: this._client.publicClient?.account,
       });
     });
